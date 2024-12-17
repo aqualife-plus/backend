9e3908c8b6e0753c12867bd41220830a8af1640a
고성태 <v12rhtjdxo@naver.com>
2024-12-17 16:01:28 +0900

    feat : co2, light, filter fcm 적용

--- src/main/java/com/aqualifeplus/aqualifeplus/common/redis/ExpiredEventListener.java
+++ src/main/java/com/aqualifeplus/aqualifeplus/common/redis/ExpiredEventListener.java
@@ -1,6 +1,11 @@
 package com.aqualifeplus.aqualifeplus.common.redis;
 
+import com.aqualifeplus.aqualifeplus.common.exception.CustomException;
+import com.aqualifeplus.aqualifeplus.common.exception.ErrorCode;
 import com.aqualifeplus.aqualifeplus.firebase.repository.FirebaseRealTimeRepository;
+import com.aqualifeplus.aqualifeplus.firebase.service.FCMService;
+import com.aqualifeplus.aqualifeplus.users.entity.Users;
+import com.aqualifeplus.aqualifeplus.users.repository.UsersRepository;
 import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,8 +20,11 @@ import org.springframework.stereotype.Component;
 public class ExpiredEventListener implements MessageListener {
     private final int ADAY = 60 * 60 * 24;
 
+    private final FCMService fcmService;
+    private final UsersRepository usersRepository;
     private final FirebaseRealTimeRepository firebaseRealTimeRepository;
     private final RedisTemplate<String, String> redisTemplateForFishbowlSettings;
+    private final RedisTemplate<String, String> redisTemplateForTokens;
 
     @Override
     public void onMessage(Message message, byte[] pattern) {
@@ -45,6 +53,15 @@ public class ExpiredEventListener implements MessageListener {
         firebaseRealTimeRepository.updateOnOff(
                 strArr[0], strArr[1], type, strArr[4].equals("on"));
         redisTemplateForFishbowlSettings.opsForValue().set(key, "", ADAY, TimeUnit.SECONDS);
+
+        Users users = usersRepository.findById(Long.parseLong(strArr[0]))
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_MEMBER));
+
+        fcmService.sendNotification(
+                redisTemplateForTokens.opsForValue().get("users : androidToken : " + users.getEmail()),
+                type,
+                type + "이 " + strArr[4] + "되었습니다."
+        );
         log.info(String.valueOf(redisTemplateForFishbowlSettings.getExpire(key, TimeUnit.SECONDS)));
     }
 
@@ -52,6 +69,15 @@ public class ExpiredEventListener implements MessageListener {
         String[] strArr = key.split("/");
         firebaseRealTimeRepository.updateFilter(strArr[0], strArr[1]);
         redisTemplateForFishbowlSettings.opsForValue().set(key, "", ADAY * 7, TimeUnit.SECONDS);
+
+        Users users = usersRepository.findById(Long.parseLong(strArr[0]))
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_MEMBER));
+
+        fcmService.sendNotification(
+                redisTemplateForTokens.opsForValue().get("users : androidToken : " + users.getEmail()),
+                "환수",
+                "환수가 시작되었습니다."
+        );
         log.info(String.valueOf(redisTemplateForFishbowlSettings.getExpire(key, TimeUnit.SECONDS)));
     }
 }
